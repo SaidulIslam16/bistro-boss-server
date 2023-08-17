fb93a400eba41a4c1f33f538ebba3f5d2a5e2c2c
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-17 20:27:01 +0600

    API Created for menu and reviews

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// bistroBoss
-// H2ACtjpSy50GbIxd
-
-
-
+// ======= MongoDB =========//
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hla3ttg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +26,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewsCollection = client.db("bistroDB").collection("reviews");
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        // reviews API
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
