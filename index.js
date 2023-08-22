2592350502431164127329646d491e244223461a
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-22 18:25:01 +0600

    created user api

--- index.js
+++ index.js
@@ -27,10 +27,19 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const usersCollection = client.db("bistroDB").collection("users");
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("cart");
 
+        // user related API
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            const result = await usersCollection.insertOne(userInfo);
+            res.send(result);
+        })
+
+        // menu API
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
