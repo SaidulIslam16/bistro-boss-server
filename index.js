188212b9b078af9be751a83532e6b023b72572b9
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-27 17:43:55 +0600

    created an api for admin stats

--- index.js
+++ index.js
@@ -205,6 +205,19 @@ async function run() {
             res.send({ result, deleteResult });
         })
 
+        app.get('/admin-stats', verrifyJWT, verfifyAdmin, async (req, res) => {
+            const users = await usersCollection.estimatedDocumentCount();
+            const products = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+
+            // best way to sum of a field is to use group and sum operator
+
+            const payments = await paymentCollection.find().toArray();
+            const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+
+            res.send({ users, products, orders, revenue });
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
